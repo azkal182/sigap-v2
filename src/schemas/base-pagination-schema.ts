365ef170591a9fbe400340a8dd195e5aa722ca5d
Azkal182 <mohazkalarif@gmail.com>
2025-08-04 02:35:24 +0700

    change limit to 10000

--- src/schemas/base-pagination-schema.ts
+++ src/schemas/base-pagination-schema.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 // Base schema untuk pagination dan sorting
 export const basePaginationSchema = z.object({
   page: z.coerce.number().min(1).default(1),
-  limit: z.coerce.number().min(1).max(100).default(10),
+  limit: z.coerce.number().min(1).max(10000).default(10),
   search: z.string().optional().default(''),
   sortOrder: z.enum(['asc', 'desc']).default('asc')
 })
